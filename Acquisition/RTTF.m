0841dd020da4435114863b9cb412e953ced81907
Joe Bell <joe.bell@gmail.com>
2011-01-10 16:31:28 -0500

    Fixed RTTF callbacks and directories

--- Acquisition/RTTF.m
+++ Acquisition/RTTF.m
@@ -54,7 +54,7 @@ end
 waitTimer = timerArray(numEvents);
 finishUpTimer = timer('ExecutionMode','singleShot',...
         'StartDelay',wholeTime,...
-        'TimerFcn', {'finishUpRTTF', wholeTime, waitTimer, trialStructureName, trialStructureList,histogramBounds,TimeRun, filename, comment});
+        'TimerFcn', {@finishUpRTTF, wholeTime, waitTimer, trialStructureName, trialStructureList,histogramBounds,TimeRun, filename, comment});
 
 start(finishUpTimer);
     
@@ -62,10 +62,44 @@ start(finishUpTimer);
 if (~isempty(recVideo))
     recordVideo(recVideo,filename,timerArray(numEvents));
 end   
-    
+  
 
 
+%%
+% This function catches the end of an RTTF to allow command line access
+% during data acquisition
+function finishUpRTTF(obj, event, wholeTime, waitTimer, trialStructureName, trialStructureList,histogramBounds,TimeRun, filename, comment)
 
+global analogIn;
+global digitalIO;
+global ardVar;
+global USB;
+global daqParams;
+
+    wait(analogIn,wholeTime+1);
+    wait(waitTimer);
+
+    ardDispOff();
+
+    acquiredData = getdata(analogIn);
+
+    data.LAmp  = acquiredData(:,1)*100;
+    data.RAmp  = acquiredData(:,2)*100;
+    data.Freq  = acquiredData(:,3)*100;
+    data.Laser = acquiredData(:,4);
+    data.Rcv   = acquiredData(:,5);
+    data.X     = acquiredData(:,6);
+    data.Odor  = acquiredData(:,7);
+
+    save([filename,'.mat'],'data', 'daqParams', 'trialStructureName','trialStructureList','histogramBounds','TimeRun');
+    disp('... Finished trial.');
+    disp(['Wrote: ',filename,'.mat']);
+    analyzeRTTF([filename,'.mat'],comment);
+
+    disp('----------------------------------------------------------');
+    disp(['WBA Diff Mean:    ', num2str(mean(data.LAmp - data.RAmp))]);
+    disp(['WBA Diff Std Dev: ', num2str(std(data.LAmp - data.RAmp))]);
+    disp('----------------------------------------------------------');
 
 
 
