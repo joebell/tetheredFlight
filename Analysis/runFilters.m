c571c0bbcfa8cad79c84fbcba0e31648fd8491bd
Joe Bell <joe.bell@gmail.com>
2012-01-09 14:43:28 -0500

    Auto-update: 09-Jan-2012 14:43:27

--- Analysis/runFilters.m
+++ Analysis/runFilters.m
@@ -7,11 +7,17 @@ function runFilters(fileList)
         sigma = 100; % 100 ms
         c = sigma;
         gaussRange = -5*sigma:5*sigma;
-        myGaussian = 1/(c*sqrt(2*pi))*exp(- (gaussRange .* gaussRange)/(2*c*c));
+        xGaussian = 1/(c*sqrt(2*pi))*exp(- (gaussRange .* gaussRange)/(2*c*c));
 
 % Design filter for WBA data
-hb=fdesign.lowpass('Fp,Fst,Ap,Ast',10,50,1,60,1000);
-db=design(hb,'equiripple');
+% hb=fdesign.lowpass('Fp,Fst,Ap,Ast',10,50,1,60,1000);
+% db=design(hb,'equiripple');
+        sigma = 100; % 100 ms
+        c = sigma;
+        gaussRange = -5*sigma:5*sigma;
+        wbaGaussian = 1/(c*sqrt(2*pi))*exp(- (gaussRange .* gaussRange)/(2*c*c));
+
+        
 
  for fileN = 1:size(fileList,2)
         
@@ -32,12 +38,13 @@ db=design(hb,'equiripple');
 % Filter X data    
 %         filtX = filtfilt(da.Numerator,1,smoothX);
 %         filtX = filtX - mean(filtX - smoothX); % Subtract any DC residual
-        filtX = conv(smoothX, myGaussian,'same');
+        filtX = conv(smoothX, xGaussian,'same');
         dX = diff(filtX).*1000;    dX(end+1) = dX(end);
         
 % Filter WBA data
-        filtDiff = filtfilt(db.Numerator,1,lTrace - rTrace);
-        filtDiff = filtDiff - mean(filtDiff - (lTrace - rTrace)); % Subtract DC
+%         filtDiff = filtfilt(db.Numerator,1,lTrace - rTrace);
+%         filtDiff = filtDiff - mean(filtDiff - (lTrace - rTrace)); % Subtract DC
+        filtDiff = conv(lTrace-rTrace, xGaussian,'same');        
         dWBAdiff = diff(filtDiff) .* 1000; dWBAdiff(end+1) = dWBAdiff(end);
         
         filteredData.filtX = filtX;
