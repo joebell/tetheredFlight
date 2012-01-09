cf787fe9c487b548e7d8f996e31b309c921874e5
Joe Bell <joe.bell@gmail.com>
2012-01-09 14:33:21 -0500

    Auto-update: 09-Jan-2012 14:33:21

--- Analysis/runFilters.m
+++ Analysis/runFilters.m
@@ -1,8 +1,13 @@
 function runFilters(fileList)
 
-% Design filter for making dX/dt data
-ha=fdesign.lowpass('Fp,Fst,Ap,Ast',1,8,1,60,1000);
-da=design(ha,'equiripple');
+% % Design filter for making dX/dt data
+% ha=fdesign.lowpass('Fp,Fst,Ap,Ast',1,8,1,60,1000);
+% da=design(ha,'equiripple');
+        sigma = 100;
+        c = sigma;
+        gaussRange = -5*sigma:5*sigma;
+        myGaussian = 1/(c*sqrt(2*pi))*exp(- (gaussRange .* gaussRange)/(2*c*c));
+
 % Design filter for WBA data
 hb=fdesign.lowpass('Fp,Fst,Ap,Ast',2,10,1,60,1000);
 db=design(hb,'equiripple');
@@ -22,18 +27,14 @@ db=design(hb,'equiripple');
         [smoothX, wrappedX] = smoothUnwrap(data.X, daqParams.xOutputCal, 0);
         lTrace = data.LAmp;
         rTrace = data.RAmp;
+
         % Filter X data    
 %         filtX = filtfilt(da.Numerator,1,smoothX);
 %         filtX = filtX - mean(filtX - smoothX); % Subtract any DC residual
-
-        sigma = 80;
-        c = 80;
-        gaussRange = -5*sigma:5*sigma;
-        myGaussian = 1/(c*sqrt(2*pi))*exp(- (gaussRange .* gaussRange)/(2*c*c));
-
         filtX = conv(smoothX, myGaussian,'same');
         dX = diff(filtX).*1000;    dX(end+1) = dX(end);
-        % Filter WBA data
+        
+% Filter WBA data
         filtDiff = filtfilt(db.Numerator,1,lTrace - rTrace);
         filtDiff = filtDiff - mean(filtDiff - (lTrace - rTrace)); % Subtract DC
         dWBAdiff = diff(filtDiff) .* 1000; dWBAdiff(end+1) = dWBAdiff(end);
