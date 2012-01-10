7a63abdd7ac125c83cf7580a85a3471e8ba8aace
Joe Bell <joe.bell@gmail.com>
2012-01-10 15:20:37 -0500

    First two pages of analyzePOLBS.m done.

--- Analysis/Utilities/Plot2DFormat.m
+++ Analysis/Utilities/Plot2DFormat.m
@@ -3,6 +3,8 @@ function plot2DFormat()
 line(xlim(),[0 0],'Color',[0 0 0]);
 line([90 90],ylim(),'Color',[0 0 0]);
 line([270 270],ylim(),'Color',[0 0 0]);
+xlabel('Angle (deg)');
+ylabel('Speed (deg/sec)');
 set(gca,'XTick',[90 270]);
 set(gca, 'YTick',[-720 -360 0 360 720]);
 grid off;
