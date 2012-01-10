d8e7ff06df02eb5fdfa757ffd4173cca48ff8744
Joe Bell <joe.bell@gmail.com>
2012-01-10 10:53:50 -0500

    Working on fitModel.m

---
+++ Analysis/fitModel.m
@@ -0,0 +1,33 @@
+%%
+function modelFun = fitModel(means, n)
+
+%% Number of terms to fit
+nExponential = 5;
+nPeriodic = 5;
+
+%% Generate the fitting object
+        terms = {};
+        coeffs = {};
+        nc = 1;
+        for p= 0:nExponential
+            for m= 1:nPeriodic
+                terms{end + 1} = ['(v^',num2str(p),')*sin(x*(2*pi/360)*',num2str(m),')'];
+                coeffs{end + 1} = ['c',num2str(nc)];
+                nc = nc + 1;
+                terms{end + 1} = ['(v^',num2str(p),')*cos(x*(2*pi/360)*',num2str(m),')'];
+                coeffs{end + 1} = ['c',num2str(nc)];
+                nc = nc + 1;
+            end
+            terms{end + 1} = ['(v^',num2str(p),')'];
+            coeffs{end + 1} = ['c',num2str(nc)];
+            nc = nc + 1;
+        end
+        ffun = fittype(terms,'coefficients',coeffs,'independent',{'x','v'});
+
+ %% Do the fit
+ 
+    % Weight by the number of points in each bin
+    options = fitoptions('Weights',n(:));
+    [cfun,gof,output] = fit([angleFit(:),speedFit(:)],dataToFit(:),ffun,options);
+        
+    
