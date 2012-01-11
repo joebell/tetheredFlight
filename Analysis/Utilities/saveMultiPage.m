dbbc5b204c854d32580a20bbd699347f1a6016a4
Joe Bell <joe.bell@gmail.com>
2012-01-11 18:18:09 -0500

    Auto-update: 11-Jan-2012 18:18:09

--- Analysis/Utilities/saveMultiPage.m
+++ Analysis/Utilities/saveMultiPage.m
@@ -10,7 +10,7 @@ for fig = 1:size(figList,2)
     set(gcf, 'InvertHardcopy','off');
     set(gcf,'Units','pixels');
     scnsize = get(0,'ScreenSize');
-    %set(gcf,'Position',[1 1 scnsize(3) scnsize(4)]);
+    set(gcf,'Position',[1 1 scnsize(3) scnsize(4)]);
     set(gcf, 'PaperUnits', 'inches');
     set(gcf, 'PaperSize', [11 8.5])
     set(gcf, 'PaperPosition', [0 0 11 8.5]);
