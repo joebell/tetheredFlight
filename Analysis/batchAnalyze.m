9e91939e5f92a2525501d4f62cd8075012d2fc20
Joe Bell <joe.bell@gmail.com>
2012-01-11 15:55:18 -0500

    Auto-update: 11-Jan-2012 15:55:18

--- Analysis/batchAnalyze.m
+++ Analysis/batchAnalyze.m
@@ -1,8 +1,8 @@
 function batchAnalyze()
 
-    analyzePOLBS('120104-g1');
-    analyzePOLBS('120104-g2');
-    analyzePOLBS('120104-g6');
+%     analyzeSOLO('120104-g1');
+%     analyzeSOLO('120104-g2');
+%     analyzeSOLO('120104-g6');
     analyzePOLBS('120104-g1-POLBS');
     analyzePOLBS('120104-g2-POLBS');
     analyzePOLBS('120104-g6-POLBS');
