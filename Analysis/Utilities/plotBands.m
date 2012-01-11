680c85c527886d07677aa003ed1b9e1683e7dc32
Joe Bell <joe.bell@gmail.com>
2012-01-11 11:36:00 -0500

    Auto-update: 11-Jan-2012 11:36:00

--- Analysis/Utilities/plotBands.m
+++ Analysis/Utilities/plotBands.m
@@ -6,5 +6,9 @@ semTrace  = std(dataMatrix,1)./sqrt(size(dataMatrix,1));
 h = area([time;time]',[(meanTrace-semTrace);(2*semTrace)]',0);
         set(h,'EdgeColor','none');
         set(h,'FaceColor','none');
-        set(h(2),'FaceColor',[.8 .8 1]);
+        if lineColor == 'r'
+            set(h(2),'FaceColor',[1 .8 .8]);
+        else    
+            set(h(2),'FaceColor',[.8 .8 1]);
+        end
 plot(time,meanTrace,'Color',lineColor,'LineWidth',1);
