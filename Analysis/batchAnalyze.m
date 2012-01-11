07a8f27f1c7f1a9b58cde78f996cbcf573b01d7b
Joe Bell <joe.bell@gmail.com>
2012-01-11 18:17:09 -0500

    Auto-update: 11-Jan-2012 18:17:09

--- Analysis/batchAnalyze.m
+++ Analysis/batchAnalyze.m
@@ -6,7 +6,7 @@ function batchAnalyze()
 %     analyzePOLBS('120104-g1-POLBS'); close all;
 %     analyzePOLBS('120104-g2-POLBS'); close all;
 %     analyzePOLBS('120104-g6-POLBS'); close all;
-    analyzePOLBS('111229-ACV-2-POLBS'); close all;
+    analyzePOLBS('111229-ACV-2-POLBS'); %close all;
 %     analyzePOLBS('111229-MeS-2-POLBS'); close all;
 %     analyzePOLBS('111229-EV-POLBS'); close all;
 
