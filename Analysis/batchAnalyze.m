a9092ec13ce6082b82594a824a07e79858d8652e
Joe Bell <joe.bell@gmail.com>
2012-01-12 14:54:38 -0500

    Running batchAnalyze()

--- Analysis/batchAnalyze.m
+++ Analysis/batchAnalyze.m
@@ -1,6 +1,8 @@
 function batchAnalyze()
 
      updateToServer('Running batchAnalyze()');
+     figure(); close all;
+     
      analyzeSOLO('120104-g1'); close all;
      analyzeSOLO('120104-g2'); close all;
      analyzeSOLO('120104-g6'); close all;
