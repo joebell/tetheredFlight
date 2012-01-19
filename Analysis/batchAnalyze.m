24ab612ce44aed7d5594181dd01617540bbb713b
Joe Bell <joe.bell@gmail.com>
2012-01-19 07:22:38 -0500

    Running batchAnalyze()

--- Analysis/batchAnalyze.m
+++ Analysis/batchAnalyze.m
@@ -6,13 +6,13 @@ function batchAnalyze()
      
 %     analyzePOLBS('120104-g1-POLBS'); close all;
 %     analyzePOLBS('120104-g2-POLBS'); close all;
-    analyzePOLBS('120104-g6-POLBS'); close all;
+%     analyzePOLBS('120104-g6-POLBS'); close all;
 %     analyzePOLBS('111229-ACV-2-POLBS'); close all;
 %     analyzePOLBS('111229-MeS-2-POLBS'); close all;
 %     analyzePOLBS('111229-EV-POLBS'); close all; 
 %     analyzeSOLO('120104-g1'); close all;
 %     analyzeSOLO('120104-g2'); close all;
-%     analyzeSOLO('120104-g6'); close all;
+     analyzeSOLO('120104-g6'); close all;
 
 
 
