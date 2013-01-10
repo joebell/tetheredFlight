aad2eb15c1697045f9c824ed9397e4bcc1f5f1f4
Joe Bell <joe.bell@gmail.com>
2013-01-10 13:40:09 -0500

    Auto-update: 10-Jan-2013 13:40:09

--- Analysis/rosePOLBS.m
+++ Analysis/rosePOLBS.m
@@ -10,253 +10,253 @@ nRangeX = size(rangeX,2);
 maxY = 8;
 
 %% First generate OL plots
-OLepochs = [1 12 13];
-
-figList{1} = figure();
-preTime = 0;
-timeStep = 1;
-
-postTime = 115;
-subplot(4,3,1:3);
-epochRanges = OLepochs(1);
-[n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
-h = pcolor(rangeT,rangeX,n);
-set(h,'EdgeColor','none');
-xlim([rangeT(1) rangeT(end)]);
-ylim([rangeX(1) rangeX(end)]);
-title('Vert. Stripe P(angle|time)');
-xlabel('Time');
-set(gca,'YTick',[90 270]);
-ylabel('Angle');
-
-postTime = 120;
-subplot(4,3,4:6);
-epochRanges = OLepochs(2);
-[n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
-h = pcolor(rangeT,rangeX,n);
-set(h,'EdgeColor','none');
-xlim([rangeT(1) rangeT(end)]);
-ylim([rangeX(1) rangeX(end)]);
-title('Box + EV P(angle|time)');
-xlabel('Time');
-set(gca,'YTick',[90 180 270 360]);
-ylabel('Angle');
-
-postTime = 120;
-subplot(4,3,7:9);
-epochRanges = OLepochs(3);
-[n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
-h = pcolor(rangeT,rangeX,n);
-set(h,'EdgeColor','none');
-xlim([rangeT(1) rangeT(end)]);
-ylim([rangeX(1) rangeX(end)]);
-title('Box + Odor P(angle|time)');
-xlabel('Time');
-set(gca,'YTick',[90 180 270 360]);
-ylabel('Angle');
-
-postTime = 115;
-epochRanges = OLepochs(1);
-subplot(4,3,10); hold on;
-[ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
-ns = ns ./ (sum(ns(1,:))/nRangeX);
-plotBands(rangeX,ns,'r');
-for i=1:size(ns,1)
-    plot(rangeX,ns(i,:));
-end
-ylim([0 maxY]);
-xlim([rangeX(1) rangeX(end)]);
-set(gca,'XTick',[90 270]);
-set(gca,'YTick',[0 maxY]);
-title('Vert. Bar P(angle)');
-
-postTime = 120;
-epochRanges = OLepochs(2);
-subplot(4,3,11); hold on;
-[ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
-ns = ns ./ (sum(ns(1,:))/nRangeX);
-plotBands(rangeX,ns,'r');
-for i=1:size(ns,1)
-    plot(rangeX,ns(i,:));
-end
-ylim([0 maxY]);
-xlim([rangeX(1) rangeX(end)]);
-set(gca,'XTick',[90 270]);
-set(gca,'YTick',[0 maxY]);
-title('Box + EV P(angle)');
-
-postTime = 120;
-epochRanges = OLepochs(3);
-subplot(4,3,12); hold on;
-[ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
-ns = ns ./ (sum(ns(1,:))/nRangeX);
-plotBands(rangeX,ns,'r');
-for i=1:size(ns,1)
-    plot(rangeX,ns(i,:));
-end
-ylim([0 maxY]);
-xlim([rangeX(1) rangeX(end)]);
-set(gca,'XTick',[90 270]);
-set(gca,'YTick',[0 maxY]);
-title('Box + Odor P(angle)');
-
-bigTitle(['Experiment: ',experiment]);
-codeStampFigure(gcf);
-
-% %% Next generate phase space plots
+% OLepochs = [1 12 13];
 % 
-figList{2} = figure();
-preTime = 0;
-OLepochs = [1 12 13];
-
-postTime = 115;
-epochRanges = OLepochs(2);
-subplot(3,3,1); hold on;
-[n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-h = pcolor(rangeX,rangedX,log(n'));
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Vert. Bar log(P(angle,speed))');
-plot2dFormat();
-
-postTime = 120;
-epochRanges = OLepochs(2);
-subplot(3,3,2); hold on;
-[n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-h = pcolor(rangeX,rangedX,log(n'));
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Box + EV log(P(angle,speed))');
-plot2dFormat();
-
-
-postTime = 120;
-epochRanges = OLepochs(3);
-subplot(3,3,3); hold on;
-[n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-h = pcolor(rangeX,rangedX,log(n'));
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Box + Odor log(P(angle,speed))');
-plot2dFormat();
-
-%
-
-
-postTime = 115;
-epochRanges = OLepochs(1);
-subplot(3,3,4);hold on;
-[means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-epochMeans{1} = means;
-epochNs{1} = n;
-h = pcolor(rangeX,rangedX,means');
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Vert. Bar <dWBA/dt(angle,speed)>');
-plot2dFormat();
-
-
-postTime = 120;
-epochRanges = OLepochs(2);
-subplot(3,3,5);hold on;
-[means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-epochMeans{2} = means;
-epochNs{2} = n;
-h = pcolor(rangeX,rangedX,means');
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Box + EV <dWBA/dt(angle,speed)>');
-plot2dFormat();
-
-
-postTime = 120;
-epochRanges = OLepochs(3);
-subplot(3,3,6);hold on;
-[means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
-epochMeans{3} = means;
-epochNs{3} = n;
-h = pcolor(rangeX,rangedX,means');
-set(h,'EdgeColor','none');
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-xlabel('Angle (deg)');
-ylabel('Speed (deg/sec)');
-title('Box + Odor <dWBA/dt(angle,speed)>');
-plot2dFormat();
-
-%
-
-postTime = 115;
-epochRanges = OLepochs(1);
-subplot(3,3,7); hold on;
-modelFun = fitModel(epochMeans{1}, epochNs{1}, rangeX, rangedX);
-epochModels{1} = modelFun;
-[X,dX] = meshgrid(rangeX,rangedX);
-responses = modelFun(X,dX);
-h = pcolor(rangeX,rangedX,responses);
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-set(h,'EdgeColor','none');
-set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
-plot2dFormat();
-title('Vert. Bar Model <dWBA/dt>');
-
-postTime = 120;
-epochRanges = OLepochs(2);
-subplot(3,3,8); hold on;
-modelFun = fitModel(epochMeans{2}, epochNs{2}, rangeX, rangedX);
-epochModels{2} = modelFun;
-[X,dX] = meshgrid(rangeX,rangedX);
-responses = modelFun(X,dX);
-h = pcolor(rangeX,rangedX,responses);
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-set(h,'EdgeColor','none');
-set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
-plot2dFormat();
-title('Box + EV Model <dWBA/dt>');
-
-postTime = 120;
-epochRanges = OLepochs(3);
-subplot(3,3,9); hold on;
-modelFun = fitModel(epochMeans{3}, epochNs{3}, rangeX, rangedX);
-epochModels{3} = modelFun;
-[X,dX] = meshgrid(rangeX,rangedX);
-responses = modelFun(X,dX);
-h = pcolor(rangeX,rangedX,responses);
-xlim([rangeX(1) rangeX(end)]);
-ylim([rangedX(1) rangedX(end)]);
-caxis([ranged2X(1) ranged2X(end)]);
-set(h,'EdgeColor','none');
-set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
-plot2dFormat();
-title('Box + Odor Model <dWBA/dt>');
-
-bigTitle(['Experiment: ',experiment]);
-codeStampFigure(gcf);
-
-save('epochModels.mat','epochModels');
-return;
+% figList{1} = figure();
+% preTime = 0;
+% timeStep = 1;
+% 
+% postTime = 115;
+% subplot(4,3,1:3);
+% epochRanges = OLepochs(1);
+% [n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
+% h = pcolor(rangeT,rangeX,n);
+% set(h,'EdgeColor','none');
+% xlim([rangeT(1) rangeT(end)]);
+% ylim([rangeX(1) rangeX(end)]);
+% title('Vert. Stripe P(angle|time)');
+% xlabel('Time');
+% set(gca,'YTick',[90 270]);
+% ylabel('Angle');
+% 
+% postTime = 120;
+% subplot(4,3,4:6);
+% epochRanges = OLepochs(2);
+% [n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
+% h = pcolor(rangeT,rangeX,n);
+% set(h,'EdgeColor','none');
+% xlim([rangeT(1) rangeT(end)]);
+% ylim([rangeX(1) rangeX(end)]);
+% title('Box + EV P(angle|time)');
+% xlabel('Time');
+% set(gca,'YTick',[90 180 270 360]);
+% ylabel('Angle');
+% 
+% postTime = 120;
+% subplot(4,3,7:9);
+% epochRanges = OLepochs(3);
+% [n, rangeX, rangeT] = accumulate2DHistogram(fileList,epochRanges, preTime, postTime, timeStep, rangeX);
+% h = pcolor(rangeT,rangeX,n);
+% set(h,'EdgeColor','none');
+% xlim([rangeT(1) rangeT(end)]);
+% ylim([rangeX(1) rangeX(end)]);
+% title('Box + Odor P(angle|time)');
+% xlabel('Time');
+% set(gca,'YTick',[90 180 270 360]);
+% ylabel('Angle');
+% 
+% postTime = 115;
+% epochRanges = OLepochs(1);
+% subplot(4,3,10); hold on;
+% [ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
+% ns = ns ./ (sum(ns(1,:))/nRangeX);
+% plotBands(rangeX,ns,'r');
+% for i=1:size(ns,1)
+%     plot(rangeX,ns(i,:));
+% end
+% ylim([0 maxY]);
+% xlim([rangeX(1) rangeX(end)]);
+% set(gca,'XTick',[90 270]);
+% set(gca,'YTick',[0 maxY]);
+% title('Vert. Bar P(angle)');
+% 
+% postTime = 120;
+% epochRanges = OLepochs(2);
+% subplot(4,3,11); hold on;
+% [ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
+% ns = ns ./ (sum(ns(1,:))/nRangeX);
+% plotBands(rangeX,ns,'r');
+% for i=1:size(ns,1)
+%     plot(rangeX,ns(i,:));
+% end
+% ylim([0 maxY]);
+% xlim([rangeX(1) rangeX(end)]);
+% set(gca,'XTick',[90 270]);
+% set(gca,'YTick',[0 maxY]);
+% title('Box + EV P(angle)');
+% 
+% postTime = 120;
+% epochRanges = OLepochs(3);
+% subplot(4,3,12); hold on;
+% [ns, rangeX] = accumulateMultiHistogram(fileList,epochRanges, preTime, postTime, rangeX);
+% ns = ns ./ (sum(ns(1,:))/nRangeX);
+% plotBands(rangeX,ns,'r');
+% for i=1:size(ns,1)
+%     plot(rangeX,ns(i,:));
+% end
+% ylim([0 maxY]);
+% xlim([rangeX(1) rangeX(end)]);
+% set(gca,'XTick',[90 270]);
+% set(gca,'YTick',[0 maxY]);
+% title('Box + Odor P(angle)');
+% 
+% bigTitle(['Experiment: ',experiment]);
+% codeStampFigure(gcf);
+% 
+% % %% Next generate phase space plots
+% % 
+% figList{2} = figure();
+% preTime = 0;
+% OLepochs = [1 12 13];
+% 
+% postTime = 115;
+% epochRanges = OLepochs(2);
+% subplot(3,3,1); hold on;
+% [n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% h = pcolor(rangeX,rangedX,log(n'));
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Vert. Bar log(P(angle,speed))');
+% plot2dFormat();
+% 
+% postTime = 120;
+% epochRanges = OLepochs(2);
+% subplot(3,3,2); hold on;
+% [n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% h = pcolor(rangeX,rangedX,log(n'));
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Box + EV log(P(angle,speed))');
+% plot2dFormat();
+% 
+% 
+% postTime = 120;
+% epochRanges = OLepochs(3);
+% subplot(3,3,3); hold on;
+% [n, rangeX, rangedX] = accumulatePhaseHistogram(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% h = pcolor(rangeX,rangedX,log(n'));
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Box + Odor log(P(angle,speed))');
+% plot2dFormat();
+% 
+% %
+% 
+% 
+% postTime = 115;
+% epochRanges = OLepochs(1);
+% subplot(3,3,4);hold on;
+% [means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% epochMeans{1} = means;
+% epochNs{1} = n;
+% h = pcolor(rangeX,rangedX,means');
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Vert. Bar <dWBA/dt(angle,speed)>');
+% plot2dFormat();
+% 
+% 
+% postTime = 120;
+% epochRanges = OLepochs(2);
+% subplot(3,3,5);hold on;
+% [means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% epochMeans{2} = means;
+% epochNs{2} = n;
+% h = pcolor(rangeX,rangedX,means');
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Box + EV <dWBA/dt(angle,speed)>');
+% plot2dFormat();
+% 
+% 
+% postTime = 120;
+% epochRanges = OLepochs(3);
+% subplot(3,3,6);hold on;
+% [means, n, rangeX, rangedX] = accumulatePhaseMeans(fileList,epochRanges, preTime, postTime, rangeX, rangedX);
+% epochMeans{3} = means;
+% epochNs{3} = n;
+% h = pcolor(rangeX,rangedX,means');
+% set(h,'EdgeColor','none');
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% xlabel('Angle (deg)');
+% ylabel('Speed (deg/sec)');
+% title('Box + Odor <dWBA/dt(angle,speed)>');
+% plot2dFormat();
+% 
+% %
+% 
+% postTime = 115;
+% epochRanges = OLepochs(1);
+% subplot(3,3,7); hold on;
+% modelFun = fitModel(epochMeans{1}, epochNs{1}, rangeX, rangedX);
+% epochModels{1} = modelFun;
+% [X,dX] = meshgrid(rangeX,rangedX);
+% responses = modelFun(X,dX);
+% h = pcolor(rangeX,rangedX,responses);
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% set(h,'EdgeColor','none');
+% set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
+% plot2dFormat();
+% title('Vert. Bar Model <dWBA/dt>');
+% 
+% postTime = 120;
+% epochRanges = OLepochs(2);
+% subplot(3,3,8); hold on;
+% modelFun = fitModel(epochMeans{2}, epochNs{2}, rangeX, rangedX);
+% epochModels{2} = modelFun;
+% [X,dX] = meshgrid(rangeX,rangedX);
+% responses = modelFun(X,dX);
+% h = pcolor(rangeX,rangedX,responses);
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% set(h,'EdgeColor','none');
+% set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
+% plot2dFormat();
+% title('Box + EV Model <dWBA/dt>');
+% 
+% postTime = 120;
+% epochRanges = OLepochs(3);
+% subplot(3,3,9); hold on;
+% modelFun = fitModel(epochMeans{3}, epochNs{3}, rangeX, rangedX);
+% epochModels{3} = modelFun;
+% [X,dX] = meshgrid(rangeX,rangedX);
+% responses = modelFun(X,dX);
+% h = pcolor(rangeX,rangedX,responses);
+% xlim([rangeX(1) rangeX(end)]);
+% ylim([rangedX(1) rangedX(end)]);
+% caxis([ranged2X(1) ranged2X(end)]);
+% set(h,'EdgeColor','none');
+% set(h,'ButtonDownFcn',{@plotTrajectory,modelFun});
+% plot2dFormat();
+% title('Box + Odor Model <dWBA/dt>');
+% 
+% bigTitle(['Experiment: ',experiment]);
+% codeStampFigure(gcf);
+% 
+% save('epochModels.mat','epochModels');
+% return;
 
 %% Generate OL-CL model comparisons
 
